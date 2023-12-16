bd6fd96d1c9c19456996128c383707825c8e423b
Michael Limiero <mike5713@gmail.com>
2023-12-15 20:27:30 -0500

    epsilon WIP (old)

---
+++ keyboards/deltawhy/epsilon_zero/keymaps/default/keymap.c
@@ -0,0 +1,20 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_asetniop(
+        KC_A,    KC_S,  KC_E, KC_T,
+        KC_N,    KC_I,  KC_O, KC_P,
+        KC_LSFT, KC_SPC
+    )
+};
